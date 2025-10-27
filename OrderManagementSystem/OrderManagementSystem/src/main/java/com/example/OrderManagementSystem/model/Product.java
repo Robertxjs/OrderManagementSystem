bdf4737a782a106558c56f249d40e23c629aac11
Robertxjs <robertrus2005@gmail.com>
2025-10-27 13:34:21 +0200

    product, sellableitem, service

---
+++ OrderManagementSystem/OrderManagementSystem/src/main/java/com/example/OrderManagementSystem/model/Product.java
@@ -0,0 +1,18 @@
+package com.example.OrderManagementSystem.model;
+
+public class Product extends SellableItem{
+    private double value;
+
+    public Product(String id, String name, double value) {
+        super(id, name);
+        this.value = value;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
